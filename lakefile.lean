fc3d1904e9d4c8f42623dde40ca7822515183f87
ElifUskuplu <euskuplu@usc.edu>
2025-01-19 22:40:18 -0500

    try

--- lakefile.lean
+++ lakefile.lean
@@ -12,14 +12,3 @@ require mathlib from git
 lean_lib «Stlc» {
   -- add any library configuration options here
 }
-
-require checkdecls from git "https://github.com/PatrickMassot/checkdecls.git"
-
-meta if get_config? env = some "dev" then
-require «doc-gen4» from git
-  "https://github.com/leanprover/doc-gen4" @ "main"
-require checkdecls from git "https://github.com/PatrickMassot/checkdecls.git"
-
-meta if get_config? env = some "dev" then
-require «doc-gen4» from git
-  "https://github.com/leanprover/doc-gen4" @ "main"
